95b6a44839aba60dc06e8298849b3255f07fe03e
Adarsh Srivastava <adarsh.srivastava@synchsoft.in>
2024-03-13 13:07:30 +0530

    Changes for Package Upgrades

--- src/routes/api.routes.js
+++ src/routes/api.routes.js
@@ -6,7 +6,6 @@ const InitialRoutes = require("./route.files/initial.route");
 const BotRoutes = require("./route.files/bots.route")
 
 module.exports = async function (fastify, opts, done) {
-
     fastify.register(InitialRoutes, {prefix: process.env.API_ROUTE});
     fastify.register(BotRoutes, {prefix: process.env.API_ROUTE});
     done()
